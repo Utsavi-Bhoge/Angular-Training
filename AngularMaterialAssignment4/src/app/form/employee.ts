081346f0aa42022732b5c2591123309e4500e554
Utsavi-Bhoge <86648422+Utsavi-Bhoge@users.noreply.github.com>
2023-01-02 22:43:16 +0530

    Add files via upload

---
+++ AngularMaterialAssignment4/src/app/form/employee.ts
@@ -0,0 +1,11 @@
+export class Employee{
+
+    fullname:string="";
+    email:string="";
+    password:string="";
+    gender:string="";
+    dateofbirth:string="";
+    mobile:string="";
+    skills:string="";
+     
+}
---
+++ AngularMaterialAssignment4/src/app/steppercomponent/stepper.ts
@@ -0,0 +1,19 @@
+export class Stepper{
+
+    fullname:string="";
+    email:string="";
+    fathername:string="";
+    mothername:string="";
+    gender:string="";
+    dateofbirth:string="";
+    mobile:string="";
+    bankaccountnumber:string="";     
+    bankname:string="";
+    ifsccode:string="";
+    internetbanking:string="";
+    mobilebanking:string="";
+    presentaddress:string="";
+    permanentaddress:string="";
+
+
+}
